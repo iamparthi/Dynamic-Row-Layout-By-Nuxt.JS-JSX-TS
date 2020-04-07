a653e7e5172df17b5bdf55ee1d6e7f1a0fd486fc
Parthiban Subramanian <parthiban.subramanian@mobinius.com>
2020-04-07 19:11:42 +0530

    chore(comments): Added line comments
    added line comments for better code understanding

--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,24 @@
 import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
+// Using Vue Bootstrap dependencies
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
 export default {
+  name: 'DynamicGridLayout',
   data() {
-    return { lines: [0], totalCol: 0, allDiv: [], dragElementId: "" };
+    return { totalCol: 0, allColumns: [], dragElementId: "" };
   },
   methods: {
+    // Add New Columnn Event
     addColumn() {
       let index = this.totalCol++;
-      this.allDiv.push(
+      // Add new column into allCoumns array
+      this.allColumns.push(
         <div
           class="resizable"
-          id={index}
+          id={this.totalCol}
           draggable="true"
           onDrag={this.onDragStart}
         >
@@ -33,25 +37,32 @@ export default {
         </div>
       );
     },
+    // Element drag start event
     onDragStart(event) {
       this.dragElementId = event.target.id;
     },
+    // Element drag over event
     onDragOver(event) {
       event.preventDefault();
     },
+    // Element drop event
     onDrop(event) {
       const draggableElement = document.getElementById(this.dragElementId);
       this.setTranslate(event.offsetX, event.offsetY, draggableElement);
     },
+    // Assign styles to dragged element
     setTranslate(xPos, yPos, el) {
       el.style.position = "fixed";
       el.style.left = xPos + "px";
       el.style.top = yPos + "px";
     },
+    // Remove column Event
     deleteDiv(ele) {
       let id = Number(ele.currentTarget.id);
-      let index = this.allDiv.findIndex((x) => x.data.attrs.id === id);
-      this.allDiv.splice(index, 1);
+      // Finding the index of selected element to remove from layout
+      let index = this.allColumns.findIndex((x) => x.data.attrs.id === id);
+      this.allColumns.splice(index, 1);
+      this.totalCol--;
     },
   },
   render(h) {
@@ -64,7 +75,7 @@ export default {
         <b-icon class="settings-icon" icon="gear-fill"></b-icon>
         <b-icon class="add-bottom-icon" icon="plus-circle-fill"></b-icon>
         <b-icon class="plus-icon" icon="plus" onClick={this.addColumn}></b-icon>
-        {this.allDiv}
+        {this.allColumns}
       </div>
     );
   },
