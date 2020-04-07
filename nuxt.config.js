034228842b89c657d666830b94c8125d3885b3f4
Parthiban Subramanian <parthiban.subramanian@mobinius.com>
2020-04-07 14:00:12 +0530

    feat(Initial commit 🎉): created nuxtjs project and added single page with jsx render method

---
+++ nuxt.config.js
@@ -0,0 +1,22 @@
+export default {
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'Meta description' }
+      ]
+    },
+    css: [
+    ],
+    build: {
+      assetsPublicPath: '/',
+      assetsSubDirectory: 'assets'
+    },
+    render: {
+      bundleRenderer: {
+        shouldPreload: (file, type) => {
+          return ['script', 'style', 'font'].includes(type)
+        }
+      }
+    }
+  }
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  data() {
+  },
+  render(h) {
+    return (
+      <div>Hello World</div>
+    );
+  },
+};
+</script>
